63932ba295a6d6d954f41ea910d31efb2fceb646
Amuse <amusesla@gmail.com>
2020-09-26 22:12:38 +0900

    12. Dynamic View into REST API Endpoint

--- cloneTweet/models.py
+++ cloneTweet/models.py
@@ -8,3 +8,6 @@ class Tweet(models.Model):
     # id = models.AutoField(primary_key=True) <- 자동으로 생성됨
     content = models.TextField(blank=True, null=True)
     img = models.FileField(upload_to="image/", blank=True, null=True)
+
+    def __str__(self):
+        return self.content
--- cloneTweet/views.py
+++ cloneTweet/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from .models import Tweet  # model class imported
 
 # Method Link
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 
 
 # Create your views here.
@@ -18,9 +18,20 @@ def home_view(request, *args, **kwargs):
 #     return HttpResponse(f"<h1>Hello {num}</h1>")
 
 
+# REST API VIEW
+# which means these datas can be consumed by javaScrip, swift, etc
+# return json data by using method - JsonResponse()
+
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data = {"id": tweet_id}
+
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+        status = 200
     except:
-        raise Http404
-    return HttpResponse(f"Hello, id={tweet_id}. Your content is {obj.content}")
+        data["message"] = "This id number doesn't exist"
+        status = 404
+
+    return JsonResponse(data, status=status)
